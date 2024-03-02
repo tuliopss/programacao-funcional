f2b6992c353b7cb3c40cbb27fe993b2bccc5401c
Túlio <tuliovictorp@hotmail.com>
2024-03-01 22:38:32 -0300

    map and filter

--- script.py
+++ script.py
@@ -13,11 +13,19 @@ def factorialImp(n):
     return prod
 
 
-qntdEnergia = lambda m:(m * pow(300000000, 2))
+qntdEnergy= lambda m:(m * pow(300000000, 2))
 
+    
 dicX = {"Rafael": [100, 200], "Maria":[50, 50]}
 dicY = {"Luiz": [300, 100], "Rubens":[150, 450]}
+query = (lambda dic1, dic2: list(dic1.keys()) + list(dic2.keys()))
+    
 
-consulta = (lambda dic1, dic2: list(dic1.keys()) + list(dic2.keys()))
 
-print(consulta(dicX, dicY))
+strawberries = [2,1,3,10,5,8]
+
+values = list(map(lambda m: 8*m, strawberries))
+
+payments = list(filter(lambda m: m<=40, values))
+
+print(payments)
